d155a34532a721c60a1ed27b9b57c40f4917a3d7
a10975300 <47908224+a10975300@users.noreply.github.com>
2022-03-09 08:55:43 +0800

    old

--- js/scripts.js
+++ js/scripts.js
@@ -3,78 +3,74 @@ window.addEventListener('DOMContentLoaded', event => {
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
 
+// 點，特效
+var isShow = false;
+function show22() {
+    if(!isShow) {
+        isShow = true;
+       document.getElementById('hide22').style.display='';
+        document.getElementById('hide33').style.display='none';
+       document.getElementById('hide44').style.display='none';
+              document.getElementById('hide55').style.display='none';
+
+       document.getElementById('show22').innerText = "活動目的";
+   }
+   else {
+       isShow = false;
+       document.getElementById('hide22').style.display='none';
+       document.getElementById('show22').innerText = "活動目的";
+   }
+}
+
+var isShow = false;
+function show33() {
+    if(!isShow) {
+        isShow = true;
+       document.getElementById('hide33').style.display='';
+       document.getElementById('hide22').style.display='none';
+       document.getElementById('hide44').style.display='none';
+              document.getElementById('hide55').style.display='none';
+
+       document.getElementById('show33').innerText = "活動規劃";
+   }
+   else {
+       isShow = false;
+       document.getElementById('hide33').style.display='none';
+       document.getElementById('show33').innerText = "活動規劃";
+   }
+}
 
 
-let tabLink = document.getElementById("tabs").querySelectorAll("a");
-let tabContents = document.getElementById("tab-inner").querySelectorAll('.form-content');
-let tabLink2 = document.getElementById("tabs2").querySelectorAll("a");
-let tabContents2 = document.getElementById("tab-inner2").querySelectorAll('.form-content');
+var isShow = false;
+function show44() {
+    if(!isShow) {
+        isShow = true;
+        document.getElementById('hide33').style.display='none';
+       document.getElementById('hide22').style.display='none';
+       document.getElementById('hide55').style.display='none';
+       document.getElementById('hide44').style.display='';
+       document.getElementById('show44').innerText = "數據觀念建立";
+   }
+   else {
+       isShow = false;
+       document.getElementById('hide44').style.display='none';
+       document.getElementById('show44').innerText = "數據觀念建立";
+   }
+}
 
-window.onload = function (){
-
-    // 啟動第一個panel
-     panelDisplay(tabLink2[0]);
-     //點擊  tab a連結
-     for(let i = 0; i < tabLink2.length; i++){
-       tabLink2[i].addEventListener('click',function(e){
-        e.preventDefault();
-         panelDisplay(this);
-         // return false;
-       });
-       
-       };
-   
-       function panelDisplay(activePanel){
-          // Do something...
-          for(let i =0;i<tabLink2.length;i++){
-                
-            //設定條件tabLink2 ==activePanel 
-            //將tablink2代入for循環中並利用 if ...else 進行條件 classList.add增加class="active" ，就是每執行一次function的時候就進行全部tablink2s增加class
-           
-              if(tabLink2[i] == activePanel){
-                tabLink2[i].classList.add("active");
-                tabContents2[i].style.display="block";
-                
-              }else{
-                tabLink2[i].classList.remove("active");
-                tabContents2[i].style.display="none";
-              }
-          }
-       };
-
-           // 啟動第一個panel
-     panelDisplay2(tabLink[0]);
-     //點擊  tab a連結
-     for(let i = 0; i < tabLink.length; i++){
-       tabLink[i].addEventListener('click',function(e){
-        e.preventDefault();
-         panelDisplay2(this);
-         // return false;
-       });
-       
-       };
-   
-       function panelDisplay2(activePanel){
-          // Do something...
-          for(let i =0;i<tabLink.length;i++){
-                
-            //設定條件tabLink ==activePanel 
-            //將tablink代入for循環中並利用 if ...else 進行條件 classList.add增加class="active" ，就是每執行一次function的時候就進行全部tablinks增加class
-           
-              if(tabLink[i] == activePanel){
-                tabLink[i].classList.add("active");
-                tabContents[i].style.display="block";
-                
-              }else{
-                tabLink[i].classList.remove("active");
-                tabContents[i].style.display="none";
-              }
-          }
-       }
-  
-     };
-
-
-
-
-
+var isShow = false;
+function show55() {
+    if(!isShow) {
+        isShow = true;
+        document.getElementById('hide33').style.display='none';
+       document.getElementById('hide22').style.display='none';
+       document.getElementById('hide44').style.display='none';
+       document.getElementById('hide55').style.display='';
+       document.getElementById('show55').innerText = "數據實戰演練";
+   }
+   else {
+       isShow = false;
+       document.getElementById('hide55').style.display='none';
+       document.getElementById('show55').innerText = "數據實戰演練";
+   }
+}
